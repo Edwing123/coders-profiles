348da6e4e3df93a0f13f284cd10fc314ca666105
Edwing123 <egarciavalle2014@gmail.com>
2021-04-18 21:26:48 -0600

    Add advances in core logic
    I have added the for-now necessary imports, also I have created
    the logic to get the config file and set aliases based on the
    development mode, and finally set the logic for creating/emptying
    the outdir directory.

--- code/src/main.ts
+++ code/src/main.ts
@@ -1,1 +1,31 @@
-// * core logic
+import * as fs from 'fs-extra'
+import * as ejs from 'ejs'
+import * as yaml from 'yaml'
+
+import * as aliases from './aliases'
+import { CONFIG_FILE_PATH, ENVIRONMENT } from './constants'
+import { choose, not } from './utils'
+import { Config, EnvItem } from './types'
+
+// core
+;(async () => {
+  // * config-based approach
+  const config: Config = yaml.parse(await fs.readFile(CONFIG_FILE_PATH, 'utf8'))
+  const mode = config.mode
+  const isDev = mode === 'dev'
+
+  const env: EnvItem = choose(isDev, ENVIRONMENT.dev, ENVIRONMENT.prod)
+  const dependenciesAlias = isDev ? aliases.dev : aliases.prod
+  const viewsAliases = aliases.views
+
+  // prepare output dirs
+  const outdir = env.outdir
+  const outdirExists = await fs.pathExists(outdir)
+
+  // if does not exists, created, otherwise empty it.
+  if (not(outdirExists)) {
+    fs.mkdir(outdir)
+  } else {
+    fs.emptyDir(outdir)
+  }
+})()
