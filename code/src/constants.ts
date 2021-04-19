e22e26787673c60f3cd71249ef6993e73129b1ee
Edwing123 <egarciavalle2014@gmail.com>
2021-04-18 21:58:40 -0600

    Add dependencies path

--- code/src/constants.ts
+++ code/src/constants.ts
@@ -2,6 +2,8 @@ import { Env } from './types'
 
 export const CONFIG_FILE_PATH = 'config.yaml'
 
+export const DEPENDENCIES_PATH = './src/dependencies'
+
 export const ENVIRONMENT: Env = {
   dev: {
     name: 'dev',
